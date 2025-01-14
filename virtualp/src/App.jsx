6d9b4b406f9de5a6ec5922627aa9f417dd5b329b
mathznxs.dev <math983839179@gmail.com>
2025-01-14 15:35:04 -0300

    1

--- virtualp/src/App.jsx
+++ virtualp/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './index.css'
 import Navbar from './components/NavBar/Navbar'
 import Hero from './components/Hero/Hero'
+import Features from './components/FeatureSection/Feature'
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <div className="max-w-7xl mx-auto px-20">
         <Hero />
       </div>
+      <Features />
 
     </>
   )
