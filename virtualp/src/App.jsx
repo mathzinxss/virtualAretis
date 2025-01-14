5545ba7488a4fe77820d27f7832ffda18cf2b0af
mathznxs.dev <math983839179@gmail.com>
2025-01-14 14:15:51 -0300

    1

---
+++ virtualp/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+import Navbar from './components/NavBar/Navbar'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
