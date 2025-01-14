d48bd7bcf3de6982eb5ef2b27a4bc1d523ec8202
mathznxs.dev <math983839179@gmail.com>
2025-01-14 15:34:36 -0300

    1

--- virtualp/src/App.jsx
+++ virtualp/src/App.jsx
@@ -8,9 +8,10 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <div className="max-w-7xl mx-auto pt-20 px-6">
+      <div className="max-w-7xl mx-auto px-20">
         <Hero />
       </div>
+
     </>
   )
 }
---
+++ virtualp/src/components/FeatureSection/Feature.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Feature = () => {
+    return (
+        <div>
+            <div>
+                <span>Recursos</span>
+                <h2>Crie seu código <span>facilmente</span></h2>
+            </div>
+        </div>
+    )
+}
+
+export default Feature
