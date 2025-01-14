7b6d4ee4c9c2fd171cb84787cd1344395b580cbd
mathznxs.dev <math983839179@gmail.com>
2025-01-14 15:16:57 -0300

    1

--- virtualp/src/App.jsx
+++ virtualp/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './index.css'
 import Navbar from './components/NavBar/Navbar'
+import Hero from './components/Hero/Hero'
+
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Hero />
+      </div>
     </>
   )
 }
